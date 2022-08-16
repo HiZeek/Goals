1a94c2cd6204fd88042e01652ebaa28a99f20c25
Isaac Idibia <idibiaisaac3@gmail.com>
2022-08-16 10:42:23 +0100

    deployed

---
+++ build/precache-manifest.3a2ab21e00cdf612242f8fd1fe325b74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c0bfe8f02ac85f5b96c",
+    "url": "/static/css/main.04c16a8c.chunk.css"
+  },
+  {
+    "revision": "2c0bfe8f02ac85f5b96c",
+    "url": "/static/js/main.2c0bfe8f.chunk.js"
+  },
+  {
+    "revision": "1aaa147a804892c77c0c",
+    "url": "/static/js/1.1aaa147a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bbc60ef8c78ed5980e7d46b44e89f60f",
+    "url": "/index.html"
+  }
+];
